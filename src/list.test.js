7991c37b0727ceab2dceaf9bdc9b740a4b959ccd
Monisa_Obili <monisa2527@gmail.com>
2022-05-25 14:52:26 +0100

    added test file

--- src/list.test.js
+++ src/list.test.js
@@ -3,37 +3,29 @@ import List from './List';
 import * as api from './api';
 jest.mock("./api");
 
-test('the data is FalconSat', async () => {
-    await expect(api.getList()).resolves.toBe('FalconSat');
-  });
-  
-  test('the fetch fails with an error', async () => {
-    await expect(api.getList()).rejects.toMatch('error');
-  });
-
-// describe("FalconSat",()=>{
-//    beforeEach(()=>jest.clearAllMocks());
+ describe("FalconSat",()=>{
+    beforeEach(()=>jest.clearAllMocks());
     
-//    it("should render racket names when api responds",async()=>{
-//        api.getList.mockResolvedValue({
-//            results:[
-//                {  flight_number: 1,
-//                    mission_name: "FalconSat",
+    it("should render racket names when api responds",async()=>{
+        api.getList.mockResolvedValue({
+            results:[
+                {  flight_number: 1,
+                    mission_name: "FalconSat",
               
 
-//             }
-//            ]
-//        })
-//         render(<List getByTestId={flight_number}/>);
-//         await waitFor(()=>{
-//             screen.getByText("FalconSat")
-//         })
-//     });
-//     it("should render error message when api fails",async()=>{
-//         api.getList.mockRejectedValue({})
-//          render(<List/>);
-//          await waitFor(()=>{
-//              screen.getByText("unable to fetch data")
-//          })
-//     });
-// });
+             }
+            ]
+        })
+         render(<List getByTestId={flight_number}/>);
+         await waitFor(()=>{
+             screen.getByText("FalconSat")
+         })
+     });
+     it("should render error message when api fails",async()=>{
+         api.getList.mockRejectedValue({})
+          render(<List/>);
+          await waitFor(()=>{
+              screen.getByText("unable to fetch data")
+          })
+     });
+ });
